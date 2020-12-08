92920a9d95072e31eee63731baba227d5229fd4f
Hardikkr <hardik.kumar18feb@gmail.com>
2020-12-09 00:18:44 +0530

    App orientation to Portrait

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:recipe_book_flutter/splash_screen.dart';
 
 void main() {
@@ -8,6 +9,11 @@ void main() {
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    SystemChrome.setPreferredOrientations([
+      DeviceOrientation.portraitUp,
+      DeviceOrientation.portraitDown,
+    ]);
+
     return MaterialApp(
       title: 'Recipe Book',
       home: SplashScreen(),
